0aaa3d91fd859f17aa35ecc7cb62e8f782635293
talkingmachine <aeyueoq@gmail.com>
2022-11-03 04:07:17 +0300

    2.1 pages components added

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+type AppProps = {
+  rentalOfferCount: number;
+}
+
+function App(props:AppProps): JSX.Element {
+  return <MainPage rentalOfferCount={props.rentalOfferCount}/>;
 }
 
 export default App;
