be344fd6aa371799db3ee71fa079555c45ee6350
talkingmachine <aeyueoq@gmail.com>
2022-11-14 09:38:14 +0300

    3.1 fixes, added lazy load

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,10 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import RouterPaths from '../../const';
-import LoginPage from '../../pages/login-page/login-page';
+import RouterPaths from '../../const/router-paths';
 import MainPage from '../../pages/main-page/main-page';
 import PropertyPage from '../../pages/property-page/property-page';
 import NotFoundPage from '../not-found-page/not-found-page';
+const LoginPage = React.lazy(() => import ('../../pages/login-page/login-page'));
 
 type AppProps = {
   rentalOfferCount: number;
@@ -14,7 +15,7 @@ function App({rentalOfferCount}:AppProps): JSX.Element {
     <BrowserRouter>
       <Routes>
         <Route path={RouterPaths.main} element={<MainPage rentalOfferCount={rentalOfferCount} />} />
-        <Route path={RouterPaths.login} element={<LoginPage/>} />
+        <Route path={RouterPaths.login} element={<Suspense fallback={<span>Loading...</span>}><LoginPage/></Suspense>} />
         <Route path={RouterPaths.offer} element={<PropertyPage/>} />
         <Route path={'*'} element={<NotFoundPage/>} />
       </Routes>
